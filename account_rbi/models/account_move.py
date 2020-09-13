5cb6db6f2d5a6b7c951f4929d7187d52238002bb
fadynoor <fady.noor@gmail.com>
2020-09-13 19:14:25 +0700

    [UPD] account_rbi: cancel custom attach receipt

--- account_rbi/models/account_move.py
+++ account_rbi/models/account_move.py
@@ -7,3 +7,14 @@ class AccountMove(models.Model):
     attachment = fields.Binary('Receipt', attachment=True)
     attachment_filename = fields.Char('Filename')
     attachment_date = fields.Date('Upload Date')
+
+    # attachment_number = fields.Integer('Number of Attachments', compute='_compute_attachment_number')
+
+    # def _compute_attachment_number(self):
+    #     attachment_data = self.env['ir.attachment'].read_group([('res_model', '=', 'account.move'), ('res_id', 'in', self.ids)], ['res_id'], ['res_id'])
+    #     attachment = dict((data['res_id'], data['res_id_count']) for data in attachment_data)
+    #     for move in self:
+    #         if move.type not in ['out_invoice', 'out_refund']:
+    #             move.attachment_number = 0
+    #         else:    
+    #             move.attachment_number = attachment.get(move.id, 0)
