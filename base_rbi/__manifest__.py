a1ad2a7e309608854f9d8223ad5a6685e4789eaa
fadynoor <fady.noor@gmail.com>
2020-09-19 00:48:53 +0700

    [ADD] base_rbi: user group for reseller

---
+++ base_rbi/__manifest__.py
@@ -0,0 +1,25 @@
+{
+  'name': 'Base RBI',
+  'author': 'sahabatdev',
+  'version': '0.1',
+  'depends': [
+    # 'nama_modul',
+    'base', 'sales_team'
+  ],
+  'data': [
+    # 'views/nama_file.xml',
+    'security/groups.xml',
+  ],
+  'qweb': [
+    # 'static/src/xml/nama_widget.xml',
+  ],
+  'sequence': 1,
+  'auto_install': False,
+  'installable': True,
+  'application': True,
+  'category': '- Arkademy Part 1',
+  'summary': 'Konfigurasi Dasar RBI',
+  'license': 'OPL-1',
+  'website': 'https://www.arkana.co.id/',
+  'description': '-'
+}
